e0bff767afd0adf64d6afd5eddff35b850f3fa76
Mayur Saxena <mayur.saxena1997@gmail.com>
2025-08-05 20:16:18 +1000

    Proper persistent directory and adding explicit firewall ports.

--- services/technitium-dns.nix
+++ services/technitium-dns.nix
@@ -6,11 +6,25 @@
   services.technitium-dns-server = {
     enable = true;
     openFirewall = true;
+    firewallUDPPorts = [
+      53
+      67 #DHCP
+    ];
+    firewallTCPPorts = [
+      53
+      5380
+    ];
+
   };
 
   environment.persistence."/persistent" = {
     directories = [
-      "/etc/dns"
+      {
+        directory = "/var/lib/private/technitium-dns-server";
+        user = "technitium-dns-server";
+        group = "technitium-dns-server";
+        mode = "0755";
+      }
     ];
   };
 }
