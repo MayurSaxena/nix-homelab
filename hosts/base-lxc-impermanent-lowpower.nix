98cb6a258bd456b5b0689453a175646ce38bc84c
Mayur Saxena <mayur.saxena1997@gmail.com>
2025-08-06 21:52:16 +1000

    one more attempt.

--- hosts/base-lxc-impermanent-lowpower.nix
+++ hosts/base-lxc-impermanent-lowpower.nix
@@ -5,9 +5,6 @@
   ...
 }: {
   imports = [
-    ./../modules/nixos/base.nix
-    ./../modules/nixos/proxmox-lxc.nix
-    ./../modules/nixos/impermanence.nix
     ./../modules/nixos/remote-builds.nix
   ];
   # Set system architecture for this host
