a91000ec4d99bbcb64ac3611b445fed63885d559
Mayur Saxena <mayur.saxena1997@gmail.com>
2025-08-06 20:34:45 +1000

    Making sure install.sh only performs some actions on hosts set up for impermanence.

--- install.sh
+++ install.sh
@@ -6,22 +6,23 @@ if [ "$(uname)" == "Darwin" ]; then
   echo "do something..."
 
 elif [ "$(uname)" == "Linux" ]; then
-  
-  # Make folders required to house base persistence files
-  mkdir -p /persistent/{etc/ssh,var/{lib/{nixos,systemd},log}}
-  # Generate SSH keys
-  systemctl start sshd-keygen.service
-  sleep 2
-  # Copy files to persistent locations - could move as well
-  mv -f /etc/ssh/ssh_host_* /persistent/etc/ssh/
-  cp /etc/machine-id /persistent/etc/
+  SSH_KEY_LOCATION="/etc/ssh/ssh_host_ed25519_key.pub"
+  if [ -d "/persistent/" ]; then
+    # Make folders required to house base persistence files
+    mkdir -p /persistent/{etc/ssh,var/{lib/{nixos,systemd},log}}
+    # Generate SSH keys
+    systemctl start sshd-keygen.service
+    sleep 2
+    # Copy files to persistent locations - could move as well
+    mv -f /etc/ssh/ssh_host_* /persistent/etc/ssh/
+    SSH_KEY_LOCATION="/persistent/etc/ssh/ssh_host_ed25519_key.pub"
+    cp /etc/machine-id /persistent/etc/
 
-  # Copy current contents of folders (don't move so we don't break running things?)
-  cp -r /var/lib/nixos/ /persistent/var/lib/nixos/
-  cp -r /var/lib/systemd/ /persistent/var/lib/systemd/
-  cp -r /var/log/ /persistent/var/log/
-
+    # Copy current contents of folders (don't move so we don't break running things?)
+    cp -r /var/lib/nixos/ /persistent/var/lib/nixos/
+    cp -r /var/lib/systemd/ /persistent/var/lib/systemd/
+    cp -r /var/log/ /persistent/var/log/
+  fi
   # Output age key for secrets decryption
-  nix shell "nixpkgs#ssh-to-age" --extra-experimental-features "nix-command flakes" --command ssh-to-age -i /persistent/etc/ssh/ssh_host_ed25519_key.pub
-
+  nix shell "nixpkgs#ssh-to-age" --extra-experimental-features "nix-command flakes" --command ssh-to-age -i $SSH_KEY_LOCATION
 fi
