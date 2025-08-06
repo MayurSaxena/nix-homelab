4774c3250714eaa92aa29d7bc690f7402352d309
Mayur Saxena <mayur.saxena1997@gmail.com>
2025-08-06 20:50:28 +1000

    Forgot to make sure keys are generated.

--- install.sh
+++ install.sh
@@ -6,13 +6,13 @@ if [ "$(uname)" == "Darwin" ]; then
   echo "do something..."
 
 elif [ "$(uname)" == "Linux" ]; then
+  # Generate SSH keys
+  systemctl start sshd-keygen.service
   SSH_KEY_LOCATION="/etc/ssh/ssh_host_ed25519_key.pub"
+  
   if [ -d "/persistent/" ]; then
     # Make folders required to house base persistence files
     mkdir -p /persistent/{etc/ssh,var/{lib/{nixos,systemd},log}}
-    # Generate SSH keys
-    systemctl start sshd-keygen.service
-    sleep 2
     # Copy files to persistent locations - could move as well
     mv -f /etc/ssh/ssh_host_* /persistent/etc/ssh/
     SSH_KEY_LOCATION="/persistent/etc/ssh/ssh_host_ed25519_key.pub"
@@ -23,6 +23,7 @@ elif [ "$(uname)" == "Linux" ]; then
     cp -r /var/lib/systemd/ /persistent/var/lib/systemd/
     cp -r /var/log/ /persistent/var/log/
   fi
+  
   # Output age key for secrets decryption
   nix shell "nixpkgs#ssh-to-age" --extra-experimental-features "nix-command flakes" --command ssh-to-age -i $SSH_KEY_LOCATION
 fi
