3d11ae7842124ea073ac452f09feb352aef82978
Mayur Saxena <mayur.saxena1997@gmail.com>
2025-08-17 13:01:38 +1000

    Fixing impermanent SSH host keys.

--- modules/nixos/impermanence.nix
+++ modules/nixos/impermanence.nix
@@ -8,17 +8,17 @@
     inputs.impermanence.nixosModules.impermanence
   ];
 
-  # services.openssh.hostKeys = [
-  #   {
-  #     bits = 4096;
-  #     path = "/persistent/etc/ssh/ssh_host_rsa_key";
-  #     type = "rsa";
-  #   }
-  #   {
-  #     path = "/persistent/etc/ssh/ssh_host_ed25519_key";
-  #     type = "ed25519";
-  #   }
-  # ];
+  services.openssh.hostKeys = [
+    {
+      bits = 4096;
+      path = "/persistent/etc/ssh/ssh_host_rsa_key";
+      type = "rsa";
+    }
+    {
+      path = "/persistent/etc/ssh/ssh_host_ed25519_key";
+      type = "ed25519";
+    }
+  ];
 
   sops.age.sshKeyPaths = ["/persistent/etc/ssh/ssh_host_ed25519_key"];
 
@@ -27,11 +27,10 @@
     "d /persistent/etc/ssh 0700 root root -"
     # If persistent copy doesn’t exist, copy the ephemeral one
     "C /persistent/etc/machine-id - - - - /etc/machine-id"
+    # This might be useful if we're upgrading a permanent system to an impermanent one
     "C /persistent/etc/ssh/ssh_host_rsa_key - - - - /etc/ssh/ssh_host_rsa_key"
     "C /persistent/etc/ssh/ssh_host_ed25519_key - - - - /etc/ssh/ssh_host_ed25519_key"
   ];
-
-  # environment.etc.machine-id.source = "/persistent/etc/machine-id";
 
   environment.persistence."/persistent" = {
     hideMounts = true;
@@ -42,10 +41,6 @@
     ];
     files = [
       "/etc/machine-id"
-      "/etc/ssh/ssh_host_ed25519_key.pub"
-      "/etc/ssh/ssh_host_ed25519_key"
-      "/etc/ssh/ssh_host_rsa_key.pub"
-      "/etc/ssh/ssh_host_rsa_key"
     ];
   };
 }
