0a9b3c29425900eabbc9d4591d0775edc7cad84d
Mayur Saxena <mayur.saxena1997@gmail.com>
2025-08-19 12:18:21 +1000

    Updating outputs for brevity, might need to change this later.

--- provisioning/outputs.tf
+++ provisioning/outputs.tf
@@ -1,13 +1,11 @@
-output "dns-server" {
-  value = {
-    id    = module.dns-server.ct_id
-    addrs = module.dns-server.ct_address
-  }
+output "nix-builder" {
+  value = "${module.nix-builder.ct_id}: ${module.nix-builder.ct_address.v4[0]} ${module.nix-builder.ct_address.v6[0]}"
 }
 
-output "nix-builder" {
-  value = {
-    id    = module.nix-builder.ct_id
-    addrs = module.nix-builder.ct_address
-  }
+output "dns-server" {
+  value = "${module.dns-server.ct_id}: ${module.dns-server.ct_address.v4[0]} ${module.dns-server.ct_address.v6[0]}"
+}
+
+output "actualbudget" {
+  value = "${module.actualbudget.ct_id}: ${module.actualbudget.ct_address.v4[0]} ${module.actualbudget.ct_address.v6[0]}"
 }
