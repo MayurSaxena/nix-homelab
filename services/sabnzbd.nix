91df89a5a2c22b847c974ab1ddb91a0cdea7042d
Mayur Saxena <mayur.saxena1997@gmail.com>
2025-08-19 13:49:48 +1000

    Documentation.

--- services/sabnzbd.nix
+++ services/sabnzbd.nix
@@ -5,10 +5,14 @@
 }: {
   services.sabnzbd = {
     enable = true;
-    openFirewall = true;
+    openFirewall = true; #TCP 8080
     user = "sabnzbd";
     group = "sabnzbd";
   };
+
+  # Have to SSH to the host and make a tunnel to localhost:8080 because the default
+  # config doesn't listen on all interfaces :(
+  # TODO: Front this with an nginx or something?
 
   users.users.sabnzbd.extraGroups = [ "lxc_share" ];
 
