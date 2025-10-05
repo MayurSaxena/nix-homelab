d106706b2478a465692f7ed3f8a70a2a3d0e1c2c
Mayur Saxena <mayur.saxena1997@gmail.com>
2025-10-05 17:36:18 +1100

    Fixing fileserver

--- hosts/files.nix
+++ hosts/files.nix
@@ -14,22 +14,21 @@
 
   sops.secrets = {
     "passwords/timemachine" = {
-      neededForUsers = true;
       sopsFile = ./../secrets/fileserver.yaml;
     };
     "passwords/msaxena" = {
-      neededForUsers = true;
       sopsFile = ./../secrets/fileserver.yaml;
     };
   };
 
   users.users.timemachine = {
-    hashedPasswordFile = config.sops.secrets."passwords/timemachine".path;
+    shell = "${pkgs.shadow}/bin/nologin";
     isNormalUser = true;
+    extraGroups = ["lxc_share"];
   };
 
   users.users.msaxena = {
-    hashedPasswordFile = config.sops.secrets."passwords/msaxena".path;
+    shell = "${pkgs.shadow}/bin/nologin";
     isNormalUser = true;
     extraGroups = ["lxc_share"];
   };
@@ -42,78 +41,116 @@
     ];
   };
 
-  services.samba = {
-    enable = true;
-    openFirewall = true;
-    settings = {
-      global = {
-        "min protocol" = "SMB2";
-        "server min protocol" = "SMB2";
+  services = {
+    samba = {
+      enable = true;
+      openFirewall = true;
+      settings = {
+        global = {
+          "min protocol" = "SMB3";
+          "server min protocol" = "SMB3";
 
-        "security" = "user";
-        "workgroup" = "WORKGROUP";
-        "netbios name" = "FILES";
-        "wide links" = "no";
-        "unix extensions" = "no";
-        "spotlight" = "yes";
-        "map to guest" = "bad user";
-        "guest account" = "nobody";
+          "security" = "user";
+          "workgroup" = "WORKGROUP";
+          "netbios name" = "FILES";
+          "wide links" = "no";
+          "unix extensions" = "no";
+          "spotlight" = "yes";
+          "map to guest" = "bad user";
+          "guest account" = "nobody";
+          "ea support" = "yes";
 
-        "vfs objects" = "recycle acl_xattr catia fruit streams_xattr";
-        "fruit:nfc_aces" = "no";
-        "fruit:aapl" = "yes";
-        "fruit:model" = "MacSamba";
-        "fruit:posix_rename" = "yes";
-        "fruit:metadata" = "stream";
-        "fruit:delete_empty_adfiles" = "yes";
-        "fruit:veto_appledouble" = "no";
-        "fruit:wipe_intentionally_left_blank_rfork" = "yes";
+          "vfs objects" = "recycle acl_xattr catia fruit streams_xattr";
+          "fruit:nfc_aces" = "no";
+          "fruit:aapl" = "yes";
+          "fruit:model" = "MacSamba";
+          "fruit:posix_rename" = "yes";
+          "fruit:metadata" = "stream";
+          "fruit:delete_empty_adfiles" = "yes";
+          "fruit:veto_appledouble" = "no";
+          "fruit:wipe_intentionally_left_blank_rfork" = "yes";
+          "fruit:encoding" = "native";
+          "fruit:zero_file_id" = "yes";
+          "fruit:resource" = "file";
 
-        "recycle:touch" = "yes";
-        "recycle:keeptree" = "yes";
-        "recycle:versions" = "yes";
-        "recycle:exclude_dir" = "tmp quarantine";
+          "recycle:touch" = "yes";
+          "recycle:keeptree" = "yes";
+          "recycle:versions" = "yes";
+          "recycle:exclude_dir" = "tmp quarantine";
 
-        "unix password sync" = "yes";
-        "passwd program" = "/run/wrappers/bin/passwd %u";
-        "passwd chat" = "*Enter\snew\s*\spassword:* %n\n *Retype\snew\s*\spassword:* %n\n *password\supdated\ssuccessfully* .";
+          "unix password sync" = "yes";
+          "passwd program" = "/run/wrappers/bin/passwd %u";
+          "passwd chat" = "*Enter\snew\s*\spassword:* %n\n *Retype\snew\s*\spassword:* %n\n *password\supdated\ssuccessfully* .";
+        };
+        "TimeCapsule" = {
+          "writeable" = "yes";
+          "available" = "yes";
+          "path" = "/media/TimeCapsule";
+          "valid users" = "timemachine";
+          "guest ok" = "no";
+          "comment" = "Backups";
+          "fruit:time machine" = "yes";
+          "vfs objects" = "catia fruit streams_xattr";
+        };
+        "NetShare" = {
+          "public" = "yes";
+          "guest ok" = "yes";
+          "path" = "/media/NetShare";
+          "wide links" = "no";
+          "directory mode" = "777";
+          "valid users" = "msaxena,nobody";
+          "comment" = "Public Network Share";
+          "create mode" = "777";
+          "writeable" = "yes";
+        };
       };
-      "TimeMachine" = {
-        "writeable" = "yes";
-        "available" = "yes";
-        "force directory mode" = "0775";
-        "path" = "/media/TimeCapsule";
-        "valid users" = "timemachine";
-        "guest ok" = "no";
-        "comment" = "Backups";
-        "create mask" = "0664";
-        "directory mask" = "0775";
-        "fruit:time machine" = "yes";
-        "force create mode" = "0664";
-        "vfs objects" = "catia fruit streams_xattr";
-      };
-      "NetShare" = {
-        "public" = "yes";
-        "guest ok" = "yes";
-        "path" = "/media/NetShare";
-        "wide links" = "no";
-        "directory mode" = "777";
-        "valid users" = "msaxena,nobody";
-        "comment" = "Public Network Share";
-        "create mode" = "777";
-        "writeable" = "yes";
+      nmbd.enable = true;
+    };
+    samba-wsdd = {
+      enable = true;
+      discovery = true;
+    };
+
+    avahi = {
+      enable = true;
+      publish.enable = true;
+      publish.userServices = true;
+      nssmdns4 = true;
+      ipv6 = true;
+      nssmdns6 = true;
+
+      # https://wiki.nixos.org/wiki/Samba#Apple_Time_Machine
+      extraServiceFiles = {
+        timecapsule = ''
+          <?xml version="1.0" standalone='no'?>
+          <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
+          <service-group>
+            <name replace-wildcards="yes">%h</name>
+            <service>
+              <type>_device-info._tcp</type>
+              <port>0</port>
+              <txt-record>model=TimeCapsule8,119</txt-record>
+            </service>
+            <service>
+              <type>_smb._tcp</type>
+              <port>445</port>
+            </service>
+            <service>
+              <type>_adisk._tcp</type>
+              <txt-record>dk0=adVN=TimeCapsule,adVF=0x82</txt-record>
+              <txt-record>sys=waMa=0,adVF=0x100</txt-record>
+            </service>
+          </service-group>
+        '';
       };
     };
   };
-
-  services.samba.nmbd.enable = true;
-  services.samba-wsdd.enable = true;
 
   # Make sure to add valid Samba users below to sync Unix password to Samba
   system.activationScripts.sambaUsers = {
     text = ''
       for user in timemachine msaxena; do
-        pw=$(${pkgs.coreutils}/bin/cat /run/secrets-for-users/passwords/$user)
+        pw=$(${pkgs.coreutils}/bin/cat /run/secrets/passwords/$user)
         printf "%s\n%s\n" "$pw" "$pw" \
           | ${pkgs.samba}/bin/smbpasswd -a -s "$user" || true
       done
