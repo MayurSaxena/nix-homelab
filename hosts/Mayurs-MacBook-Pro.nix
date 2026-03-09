6ee657e5adaa33ddb1ba4d1f08d4cea3a676a2cc
Mayur Saxena <mayur.saxena1997@gmail.com>
2026-03-09 19:37:35 +1100

    Part of remote building for Linux testing on Mac.

--- hosts/Mayurs-MacBook-Pro.nix
+++ hosts/Mayurs-MacBook-Pro.nix
@@ -12,7 +12,7 @@
   # Set the platform for this host
   nixpkgs.hostPlatform = inputs.nixpkgs.lib.mkDefault "aarch64-darwin";
 
-  custom.remote-builds.enable = true;
+  custom.remote-builds-mac.enable = true;
 
   # Use home-manager to manage the user configs
   home-manager = {
