19a452bed15ab4bd9b07f22535df3e6e8321168d
Mayur Saxena <mayur.saxena1997@gmail.com>
2026-05-07 20:20:22 +1000

    Updating caddy hash.

--- hosts/caddy.nix
+++ hosts/caddy.nix
@@ -26,7 +26,7 @@ in {
     enable = true;
     package = pkgs.caddy.withPlugins {
       plugins = ["github.com/caddy-dns/cloudflare@v0.2.3"];
-      hash = "sha256-20o+14cn/eeLuf1c8uGE1ODRZGC0oxocaIVlv4tFSvA=";
+      hash = "sha256-+htYZclHv9qI0TeHcBFvPkWzJVAZ5jqzTODrh4YmqXY=";
     };
     environmentFile = config.sops.secrets."caddy-secrets".path;
     # Uncomment for development.
